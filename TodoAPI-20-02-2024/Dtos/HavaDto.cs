075e0ed79ab2c6757a783c47a089faf7cefa9b27
Emine <emineclsknn17@gmail.com>
2024-02-20 12:17:10 +0300

    Add project files.

---
+++ TodoAPI-20-02-2024/Dtos/HavaDto.cs
@@ -0,0 +1,12 @@
+﻿namespace TodoAPI_20_02_2024.Dtos
+{
+    public class HavaDto
+    {
+        public DateOnly Tarih { get; set; }
+
+        public int Sicaklik { get; set; } 
+
+        public string Aciklama { get; set; } = null!;
+
+    }
+}
---
+++ TodoAPI-20-02-2024/Program.cs
@@ -0,0 +1,35 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+
+
+
+app.UseHttpsRedirection();
+
+
+//dizinlerde index html default yapýlýr
+app.UseDefaultFiles();
+//static dosyalarýn dýþarýya açýlmasý (wwwroot)
+app.UseStaticFiles();
+
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
